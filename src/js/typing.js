9c40fdaf92510fa46ceddab9fe79cb1d46148082
Adam Chen <adam.y.chen@gmail.com>
2023-06-29 22:48:48 -0700

    Updating Resume Information

--- src/js/typing.js
+++ src/js/typing.js
@@ -2,7 +2,11 @@ new TypeIt("#typewriter", {
   speed: 50,
   loop: true
 }).pause(750)
-  .type("Computer Science Student", {delay: 2000})
+  .type("Computer Science Major", {delay: 2000})
   .delete(null, {delay: 1000})
-  .type("Junior Developer", {delay: 2000})
+  .type("Data Science Minor", {delay: 2000})
+  .delete(null, {delay: 1000})
+  .type("Software Developer", {delay: 2000})
+  .delete(null, {delay: 1000})
+  .type("Data Analyst", {delay: 2000})
   .go();
